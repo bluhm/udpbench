79aeb8b3f971a4b9a3398c96f76af0f725872536
Alexander Bluhm <alexander.bluhm@gmx.net>
2022-07-30 02:11:58 +0200

    Bump copyright year.

--- udpbench.c
+++ udpbench.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019 Alexander Bluhm <bluhm@genua.de>
+ * Copyright (c) 2019-2022 Alexander Bluhm <bluhm@genua.de>
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
